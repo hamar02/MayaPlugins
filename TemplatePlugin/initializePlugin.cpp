ee843e93b94957eb08d6e664e3a73ba90698d712
Unknown <hampuz.aronsson@gmail.com>
2019-11-26 23:42:10 +0100

    Added MpxGPUDeformer

--- TemplatePlugin/initializePlugin.cpp
+++ TemplatePlugin/initializePlugin.cpp
@@ -1,16 +1,16 @@
-#include "TemplatePlugin.h"
+#include "MPxGPUDeformerTemplatePlugin.h"
 #include <maya/MFnPlugin.h>
 
 MStatus initializePlugin(MObject obj) {
 	MStatus status;
 	MFnPlugin plugin(obj, "My plug-in", "1.0", "Any");
-	status = plugin.registerNode("TemplatePlugin", TemplatePlugin::id, TemplatePlugin::creator, TemplatePlugin::initialize);
+	status = plugin.registerNode("MPxGPUDeformerTemplatePlugin", MPxGPUDeformerTemplatePlugin::id, MPxGPUDeformerTemplatePlugin::creator, MPxGPUDeformerTemplatePlugin::initialize);
 	return status;
 }
 
 MStatus uninitializePlugin(MObject obj) {
 	MStatus status;
 	MFnPlugin plugin(obj);
-	status = plugin.deregisterNode(TemplatePlugin::id);
+	status = plugin.deregisterNode(MPxGPUDeformerTemplatePlugin::id);
 	return status;
 }
